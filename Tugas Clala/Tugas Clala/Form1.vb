5c3c773148145759c37165eb9125878b59a60e63
Clala <santaclala4@gmail.com>
2015-10-06 07:59:05 +0700

    Mengubah warna background

---
+++ Tugas Clala/Tugas Clala/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+
+    Private Sub Latian_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackColor = Color.Violet
+    End Sub
+
+    Private Sub ubah_Click_1(sender As Object, e As EventArgs) Handles ubah.Click
+        If Me.BackColor = Color.Violet Then
+            Me.BackColor = Color.Salmon
+        Else
+            Me.BackColor = Color.Violet
+
+        End If
+    End Sub
+End Class
